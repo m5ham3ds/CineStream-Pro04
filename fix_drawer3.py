c08e77f263ea9a73f3e4fe88ffd30c0cd232b421
M O H A M E D <shadowprom@outlook.com>
2026-09-12 03:42:03 +0200

    chore: remove temporary migration and fix scripts
    Cleanup development utility scripts used for temporary refactoring tasks.

--- fix_drawer3.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open('app/src/main/java/com/example/navigation/AppNavigation.kt', 'r') as f:
-    text = f.read()
-
-def repl(match):
-    # This regex is too hard to get right. 
-    pass
